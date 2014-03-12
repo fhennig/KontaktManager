e5103f1d3a081a8137eeaf4f162e729c900487f1
Ryrask <muellid@live.de>
2014-03-12 16:55:05 +0100

    Test database method.

--- KontaktManager/src/kontaktmngr/dal/Connection.java
+++ KontaktManager/src/kontaktmngr/dal/Connection.java
@@ -1,8 +1,21 @@
 package kontaktmngr.dal;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import kontaktmngr.model.Person;
+
 public class Connection
 {
 	private static Connection _instance;
+	private static String _host;
+	private static String _user;
+	private static String _password;
 	
 	public static Connection getInstance()
 	{
@@ -11,4 +24,47 @@ public class Connection
 		
 		return _instance;
 	}
+	
+	private java.sql.Connection connection;
+	
+	private Connection()
+	{
+		//Einfachheitshalber...
+		_host = JOptionPane.showInputDialog("Server-IP");
+		_user = JOptionPane.showInputDialog("Username");
+		_password = JOptionPane.showInputDialog("Password");
+	}
+	
+	private void open() throws SQLException
+	{
+		if (connection == null || connection.isClosed())
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://" + _host + ":5432/Kontaktmngr",
+					_user, _password);
+	}
+	
+	private List<Person> getPersonsFirstData() throws SQLException
+	{
+		open();
+		ResultSet rs = connection.createStatement().executeQuery(
+				"select id, forename, surname from persons order by forename;");
+		List<Person> result = new ArrayList<Person>();
+		while (rs.next()) {
+			Person person = new Person(rs.getInt(1));
+			person.set_forename(rs.getString(2));
+			person.set_surname(rs.getString(3));
+			result.add(person);
+		}
+		connection.close();
+		return result;
+	}
+	
+	public static void main(String[] args) throws SQLException {
+		Connection c = Connection.getInstance();
+		List<Person> persons = c.getPersonsFirstData();
+		for (Person person : persons) {
+			System.out.println(person);
+		}
+	}
+	
 }
--- KontaktManager/src/kontaktmngr/model/Person.java
+++ KontaktManager/src/kontaktmngr/model/Person.java
@@ -1,6 +1,7 @@
 package kontaktmngr.model;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Person extends Entity
@@ -11,10 +12,61 @@ public class Person extends Entity
 	private final List<Person> _relationships = new ArrayList<>();
 	//List Category
 	//Person fields
+	//gender: char oder boolean?
+	private String _title;
+	private String _forename;
+	private String _surname;
+	private String _nickname;
+	private Calendar _birthday;
 	
 	public Person(int id)
 	{
 		super(id);
 		// TODO Auto-generated constructor stub
+	}
+
+	public String get_title() {
+		return _title;
+	}
+
+	public void set_title(String _title) {
+		this._title = _title;
+	}
+
+	public String get_forename() {
+		return _forename;
+	}
+
+	public void set_forename(String _forename) {
+		this._forename = _forename;
+	}
+
+	public String get_surname() {
+		return _surname;
+	}
+
+	public void set_surname(String _surname) {
+		this._surname = _surname;
+	}
+
+	public String get_nickname() {
+		return _nickname;
+	}
+
+	public void set_nickname(String _nickname) {
+		this._nickname = _nickname;
+	}
+
+	public Calendar get_birthday() {
+		return _birthday;
+	}
+
+	public void set_birthday(Calendar _birthday) {
+		this._birthday = _birthday;
+	}
+	
+	public String toString()
+	{
+		return getId() + ": " + _forename + " " + _surname;
 	}
 }
